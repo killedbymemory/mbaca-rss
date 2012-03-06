e0634dbb552d26e1f10a0df550e3f30fdf2dbb57
Leonardo Situmorang <leonardo@situmorang.net>
2012-03-07 02:16:26 +0700

    kick off

---
+++ src/net/situmorang/leonardo/mbaca_rss/MainActivity.java
@@ -0,0 +1,13 @@
+package net.situmorang.leonardo.mbaca_rss;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
